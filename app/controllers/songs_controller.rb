8124268467e9eaf8b5bdeef6c3c4caa093cf8a3d
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-13 17:50:28 -0400

    commit tree b4150afa73a0967d7658f7cebf8748caedbae6bc

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,39 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      
+      artist = Artist.find_by(id: params[:artist_id])
+      
+      if artist
+        @songs = artist.songs
+      else
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      song = artist.songs.find_by(id: params[:id])
+    
+      if song && artist
+        @song = song
+      elsif !song
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path(artist)
+      else
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      end
+
+    else
+      @song = Song.find_by(id: params[:id])
+    end
+
   end
 
   def new
